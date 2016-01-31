c84efef047115b68421cc01e0f1e54f0a8e831ed
Andrew Berger <andrewjberger@gail.com>
2016-01-31 01:54:07 -0800

    Rewrote frame generation section to increase speed. Now frames are generated by jumping to specific times instead of via processing the whole video.
    Removed ffmpeg's verbose output.
    
    Added more meaningful human-readable output.
    
    Font size and placement is now scaled to be proportionate to video width and height to better handle smaller videos.
    
    Removed all references to thumbnails since the images generated are too large to be considered thumbnails.
    
    Came up with workaround to fix issue where VLC drops the last frame in the image sequence video.

--- extract-frames.sh
+++ extract-frames.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Generates thumbnails from a video file by extracting still frames at a regular interval. Each thumbnail is stamped with the time elapsed since the beginning of the file. Also generates a playable video file from the thumbnails. Thumbnails and video are placed in their own output directory.
+# Generates still images from a video file by extracting frames at a regular interval. Each image is stamped with the time elapsed since the beginning of the file. Also generates a playable video file from the images. Images and video are placed in their own output directory.
 #
 # Usage: 
 #
 # This script takes three arguments:
 #
-# timestamp-thumbs.sh path/to/file path/to/output/directory interval
+# extract-frames.sh path/to/file path/to/output/directory interval
 
 input_file=$1
 output_directory=$2
@@ -19,37 +19,64 @@ then
     exit
 fi
 
-# Calculate offset for timestamp of first extracted frame. ffmpeg takes the first frame at half of the interval you set.
+# Calculate offset for the timestamp of first extracted frame. Instead of starting with the first frame, start at half of the selected interval.
 # Example: if interval=20 seconds, the first frame will be from 10 seconds into the file
 offset=$(echo "scale=3;($interval / 2)" | bc) # Use decimal places to account for odd-numbered intervals
 
-# Calculate number of frames to extract. If the number of frames is not set, ffmpeg will extract the final frame of the video even if it does not match the interval you've set.
-#TODO: check math
+# Calculate number of frames to extract based on interval and duration.
 duration=$(ffprobe -i "$input_file" -v error -show_entries format=duration -of default=nokey=1:noprint_wrappers=1)
 frames=$(echo "scale=0;($duration + $offset)/$interval" | bc) # Needs to be a whole number
 
-# If no thumbnails will be created, alert the user and quit.
+# If no images will be created, alert the user and quit.
 if [ "$frames" == 0 ]
 then
-    echo "No thumbnails will be created. Please check if your interval is too long for this file."
+    echo "No images will be created. Please check if your interval is longer than the file."
     exit
 fi
 
-# Create output directory
+# Create output directory. To be safe, make sure this is a new directory.
 if [ -d "$output_directory" ]
 then
     echo "A folder with the name $output_directory already exists. Please choose a new location."
     exit
 else
-    mkdir -pv "$output_directory"/thumbnails
+    printf "\nCreating output directory.\n"
+    mkdir -pv "$output_directory"/frames
 fi
 
-# Create the thumbnails
+printf "\nProcessing $input_file:\n"
+
+# Extract the frames
 # Exact path to font file will depend on your system configuration.
-# To consider: make the thumbnail file format and size configurable from the command line
-ffmpeg -i "$input_file" -filter_complex "fps=1/$interval,scale=w='min(960\, iw):h=-1',drawtext=fontfile=/usr/share/fonts/truetype/freefont/FreeSans.ttf: text='%{pts \\: hms \\: $offset}': x=100: y=h-100: fontsize=24: fontcolor=yellow@0.8: box=1: boxcolor=blue@0.9" -vframes $frames "$output_directory"/thumbnails/thumb%04dtimestamped.jpg
+# This uses a loop to extract to extract frames at specific points in the video via seeking to exact times. This is much faster than the original method used in this script, which relied on ffmpeg to process the entire file.
+printf "Extracting frames:\n"
+
+# Calculate font size based on height of video, with a maximum of 24
+height=$(ffprobe -i "$input_file" -v error -show_entries stream=height -of default=nokey=1:noprint_wrappers=1) 
+font_size=$(($height/20))
+if [ $font_size -gt 24 ]
+then
+    font_size=24
+fi
+
+# Extract frames
+i=0
+while [ $i -lt $(($frames)) ]
+do
+    jump=$(echo "scale=3;x=$i*$interval+$offset;if(x < 1) print 0;x" | bc) # ffmpeg seek requires leading zero for values < 1
+    paddedi=$(printf "%04d" $i) # for padding frame filenames
+    ffmpeg -loglevel error -ss $jump -i "$input_file" -filter_complex "scale=w='min(960\, iw):h=-2',drawtext=fontfile=/usr/share/fonts/truetype/freefont/FreeSans.ttf: text='%{pts \\: hms \\: $jump} ' : x=w/10: y=h-(h/10): fontsize=$font_size: fontcolor=yellow@0.8: box=1: boxcolor=blue@0.9" -vframes 1 "$output_directory"/frames/frame"$paddedi".jpg
+    printf "Extracting $(($i+1))/$frames\r"
+    i=$(($i + 1))
+done
+
+# Workaround for VLC playback. VLC doesn't seem to play the final frame in the image sequence video. Duplicate first frame to add one final frame so that the real final frame appears if played on VLC. There may be a better way to do this.
+# Note: ffplay will show all frames, including the fake final frame.
+fakefinalframe=$(printf "%04d" $frames)
+cp "$output_directory"/frames/frame0000.jpg "$output_directory"/frames/frame"$fakefinalframe".jpg
 
 # Make a video from the image sequence  
-ffmpeg -framerate 3/1 -f image2 -i "$output_directory"/thumbnails/thumb%04dtimestamped.jpg -an -vf fps=25 "$output_directory"/"$(basename "${input_file%.*}")-thumbs.mkv"
+printf "\nFinished extracting frames.\nCreating image sequence video:\n"
+ffmpeg -loglevel error -stats -f image2 -framerate 3/1 -i "$output_directory"/frames/frame%04d.jpg -an -vf fps=25 "$output_directory"/"$(basename "${input_file%.*}")-frames.mkv"
 
-echo "Processing is complete. The new files are located at $output_directory"
+printf "Processing is complete. The new files are located at $output_directory\n"
