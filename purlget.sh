9efa04c40f6c1687ddf384b82bd894bcb7618ff4
Andrew Berger <andrewjberger@gmail.com>
2021-04-09 01:12:21 -0700

    Added ability to dowload from only certain resource types.

--- purlget.sh
+++ purlget.sh
@@ -4,6 +4,7 @@
 # note: the actual files have a base URL of stacks.stanford.edu
 
 purl=$1
+resource_type=$2
 
 if [ -z "$purl" ]
 then
@@ -11,14 +12,34 @@ then
 	exit
 fi
 
+if [ ! -z "$resource_type" ]
+then
+	resource_type="[@type='$resource_type']"
+#	echo "$resource_type"
+fi
+
 druid=$(basename "$purl")
+if [ "$druid" == "$purl" ]
+then
+#	echo "maybe you forgot the url?"
+	purl="https://purl.stanford.edu/$druid"
+fi
+
 stacks_base="https://stacks.stanford.edu/file/$druid"
+
+
+mkdir -v "$druid" || exit # don't download if folder already exists
 
 # download purl xml to get filenames for download
 purl_xml=$(curl -gs "${purl}.xml")
+contentMetadata=$(echo "$purl_xml" | xmlstarlet sel -t -m "/publicObject/contentMetadata" -c . )
 
-echo "$purl_xml" | xmlstarlet sel -t -m '/publicObject/contentMetadata/resource/file/@id' -v "." -n | 
+filenames=$(echo "$purl_xml" |
+	xmlstarlet sel -t -m "/publicObject/contentMetadata/resource${resource_type}/file/@id" -v "." -n)
+
+echo "$filenames" |	
+
 	while read filename
 	do
-		wget "$stacks_base/$filename"
+		wget -P "$druid" "$stacks_base/$filename"
 	done
