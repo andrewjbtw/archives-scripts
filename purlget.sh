4205a422cd1bbc5cdb5eabf3c8c9036b40d6a15e
Andrew Berger <andrewjberger@gmail.com>
2022-03-05 23:15:24 -0800

    Add usage note, switch options, xml-only option.

--- purlget.sh
+++ purlget.sh
@@ -3,43 +3,76 @@
 # script to download files hosted via Stanford's "purl" permanent URL scheme
 # note: the actual files have a base URL of stacks.stanford.edu
 
-purl=$1
-resource_type=$2
+purl_base="https://purl.stanford.edu"
+stacks_base="https://stacks.stanford.edu/file"
+
+usage ()
+{
+    echo 'Usage : purlget.sh -p <purl> [-r <resource_type>] [--xml-only]'
+    exit
+}
+
+if [ "$#" -eq 0 ]
+then
+    usage
+    exit
+fi
+
+while [ "$1" != "" ]
+do
+    case "$1" in
+        -p )    shift
+                purl=$1
+                ;;
+        -r )    shift
+                resource_type=$1
+                ;;
+        --xml-only )
+                xml_only=true
+                ;;
+    esac
+    shift
+done
 
 if [ -z "$purl" ]
 then
-	echo "no purl, no download"
-	exit
+    echo "Please enter a druid or a purl"
+    usage
+    exit
 fi
 
-if [ ! -z "$resource_type" ]
-then
-	resource_type="[@type='$resource_type']"
-#	echo "$resource_type"
-fi
-
+# handle druids or full URLs
 druid=$(basename "$purl")
 if [ "$druid" == "$purl" ]
 then
-#	echo "maybe you forgot the url?"
-	purl="https://purl.stanford.edu/$druid"
+	purl="$purl_base/$druid"
 fi
 
-stacks_base="https://stacks.stanford.edu/file/$druid"
+# skip downloading files if give the xml-only option
+# this option overrides all other options
+if [ "$xml_only" == "true" ]
+then
+	curl -gs "${purl}.xml"
+else
+	# select only files from the given resource type
+	if [ -n "$resource_type" ]
+	then
+		resource_type="[@type='$resource_type']"
+	#	echo "$resource_type"
+	fi
 
+	mkdir -v "$druid" || exit # don't download if folder already exists
 
-mkdir -v "$druid" || exit # don't download if folder already exists
+	# download purl xml to get filenames for download
+	purl_xml=$(curl -gs "${purl}.xml")
+	contentMetadata=$(echo "$purl_xml" | xmlstarlet sel -t -m "/publicObject/contentMetadata" -c . )
 
-# download purl xml to get filenames for download
-purl_xml=$(curl -gs "${purl}.xml")
-contentMetadata=$(echo "$purl_xml" | xmlstarlet sel -t -m "/publicObject/contentMetadata" -c . )
+	filenames=$(echo "$purl_xml" |
+		xmlstarlet sel -t -m "/publicObject/contentMetadata/resource${resource_type}/file/@id" -v "." -n)
 
-filenames=$(echo "$purl_xml" |
-	xmlstarlet sel -t -m "/publicObject/contentMetadata/resource${resource_type}/file/@id" -v "." -n)
-
-echo "$filenames" |	
-
-	while read filename
-	do
-		wget -P "$druid" "$stacks_base/$filename"
-	done
+	echo "$filenames" |
+		while read filename
+		do
+			wget -P "$druid" "$stacks_base/$druid/$filename"
+		done
+fi
